6972966aa6d4d58a95fdbb14fa0ddbb10aef621c
Aks289 <65862195+Aks289@users.noreply.github.com>
2026-04-16 11:32:00 +0530

    Delete tools directory

--- tools/input_selector.py
+++
@@ -1,9 +0,0 @@
-def choose_input_source():
-    print("\n📥 Select Input Source:")
-    print("1. Email")
-    print("2. Google Drive")
-    print("3. Local Files")
-
-    choice = input("Enter choice (1/2/3): ")
-
-    return choice
